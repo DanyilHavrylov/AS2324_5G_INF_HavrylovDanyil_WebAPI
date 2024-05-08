29158432614c0640bd85330bbd41a20fd447b2c6
DanyilHavrylov <havrylov.github@gmail.com>
2024-05-08 08:37:05 +0200

    Creazione del HttpGet("AnnoBisestile")

--- Controllers/MathController.cs
+++ Controllers/MathController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using AS2324_5G_INF_HavrylovDanyil_WebAPI.Models;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Numerics;
+using System;
 
 namespace AS2324_5G_INF_HavrylovDanyil_WebAPI.Controllers
 {
@@ -20,6 +22,22 @@ namespace AS2324_5G_INF_HavrylovDanyil_WebAPI.Controllers
             {
                 status_result = "KO";
                 message = "Il primo numero non è un multiplo intero del secondo";
+            }
+
+            return Json(new { output = result, status = status_result, message = message });
+        }
+
+        [HttpGet("AnnoBisestile")]
+        public JsonResult AnnoBisestile(int anno)
+        {
+            string status_result = "OK";
+            string message = "L'anno è bisestile";
+            bool result = anno % 4 == 0 && (anno % 100 != 0 || anno % 400 == 0);
+
+            if (result == false)
+            {
+                status_result = "KO";
+                message = "L'anno non è bisestile";
             }
 
             return Json(new { output = result, status = status_result, message = message });
