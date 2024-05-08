79b4754284fcfd1252b4dcbc055b351f9c7541a4
DanyilHavrylov <havrylov.github@gmail.com>
2024-05-08 08:19:03 +0200

    Creazione del modello Numbers e controller MathController

--- Controllers/MathController.cs
+++ Controllers/MathController.cs
@@ -7,11 +7,5 @@ namespace AS2324_5G_INF_HavrylovDanyil_WebAPI.Controllers
     [ApiController]
     public class MathController : ControllerBase
     {
-        [HttpGet]
-        public JsonResult MultiploIntero(int num, int factor)
-        {
-            var result = num % factor == 0;
-            return new JsonResult(new { output = result, status = result? "OK" : "KO", message = result? "Il primo numero è un multiplo intero del secondo" : "Il primo numero non è un multiplo intero del secondo" });
-        }
     }
 }
