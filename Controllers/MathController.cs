52b970835838e2b29399296d2b1de02da6d009e9
DanyilHavrylov <havrylov.github@gmail.com>
2024-05-08 08:17:09 +0200

    Add project files.

---
+++ Controllers/MathController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AS2324_5G_INF_HavrylovDanyil_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult MultiploIntero(int num, int factor)
+        {
+            var result = num % factor == 0;
+            return new JsonResult(new { output = result, status = result? "OK" : "KO", message = result? "Il primo numero è un multiplo intero del secondo" : "Il primo numero non è un multiplo intero del secondo" });
+        }
+    }
+}
